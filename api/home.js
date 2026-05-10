123531bf659b519240c17d7e61e31c401f36873e
mikejamesla <mikejfakes@gmail.com>
2026-05-09 23:40:36 -0400

    Remove leftover phone CTA from homepage response

--- api/home.js
+++ api/home.js
@@ -6,6 +6,7 @@ module.exports = function handler(req, res) {
   let html = fs.readFileSync(indexPath, 'utf8');
 
   html = html.replace(/\n?<!-- ===================== FOOTER ===================== -->[\s\S]*?<\/footer>\n?/i, '\n');
+  html = html.replace(/\n?\s*<a href="tel:\+13017099821" class="hero-cta"[^>]*>Call For Info: \(301\) 709-9821[^<]*<\/a>\n?/i, '\n');
 
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate=300');
